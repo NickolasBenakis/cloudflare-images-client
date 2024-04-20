725db97e56cb16d062df973720401fdc23992ccc
Nikolaos Benakis <nickolasbenele@gmail.com>
2024-04-20 15:32:58 +0300

    Fix test

--- tests/cloudflareClient.test.ts
+++ tests/cloudflareClient.test.ts
@@ -1,20 +1,32 @@
+import { describe } from "node:test";
 import { expect, test } from "vitest";
 import { cloudflareClient } from "../src/index";
 
-test("uploadImage From Url", async () => {
-	const response = await cloudflareClient.uploadImageFromUrl({
-		imageUrl:
-			"https://images.squarespace-cdn.com/content/v1/60f1a490a90ed8713c41c36c/1629223610791-LCBJG5451DRKX4WOB4SP/37-design-powers-url-structure.jpeg",
-		metadata: {},
+describe("uploadImage From Url", async () => {
+	test("success response", async () => {
+		const response = await cloudflareClient.uploadImageFromUrl({
+			imageUrl:
+				"https://images.squarespace-cdn.com/content/v1/60f1a490a90ed8713c41c36c/1629223610791-LCBJG5451DRKX4WOB4SP/37-design-powers-url-structure.jpeg",
+			metadata: {},
+		});
+
+		expect(response.errors).toEqual([]);
+		expect(response.messages).toEqual([]);
+		expect(response.success).toBe(true);
+		expect(response.result.filename).toBeTypeOf("string");
+		expect(response.result.id).toBeTypeOf("string");
+		expect(response.result.meta).toEqual({});
+		expect(response.result.requireSignedURLs).toBe(false);
+		expect(response.result.uploaded).toBeTypeOf("string");
+		expect(response.result.variants.length).toBeGreaterThan(0);
 	});
 
-	expect(response.errors).toEqual([]);
-	expect(response.messages).toEqual([]);
-	expect(response.success).toBe(true);
-	expect(response.result.filename).toBeTypeOf("string");
-	expect(response.result.id).toBeTypeOf("string");
-	expect(response.result.meta).toEqual({});
-	expect(response.result.requireSignedURLs).toBe(false);
-	expect(response.result.uploaded).toBeTypeOf("string");
-	expect(response.result.variants.length).toBeGreaterThan(0);
+	test("error response", async () => {
+		const response = await cloudflareClient.uploadImageFromUrl({
+			imageUrl: "",
+			metadata: {},
+		});
+
+		expect(response.errors.length).toBeGreaterThan(0);
+	});
 });
